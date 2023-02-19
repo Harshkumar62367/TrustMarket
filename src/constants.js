f4ed6065ff920eea99de727b3b9e90f477b03c2d
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2023-02-19 14:22:27 +0545

    Updated deploy script and deployed contracts

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+//Deployed Address
+
+export const TrustMarketplaceAddress = '0x6c7519B89DD823007AF39947F0d13D577F97e494';
+export const TrustNFTAddress = '0x62c81025bD18B8e655BD2A5783944Ea1fF23bf2A';
+export const TrustDaoNFTAddress = '0x3d4d72c069D2736Bb486B134Bc07cd25D30951eF';
+export const EscrowAddress = '0x31a41b5ce47FFe8e5DAbc16884fF36090cAB62A1';
+export const TrustDao = '0x22a9242F237A5698eb76d96a69D6bc14D6819677';
